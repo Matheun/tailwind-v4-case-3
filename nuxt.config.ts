736e95ed1816191c8397e4ab8433ce0d8771c0b1
Matheun Struik <77466034+Matheun@users.noreply.github.com>
2024-09-11 14:29:04 +0200

    Added example of HMR breaking issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 export default defineNuxtConfig({
   css: ["./assets/style/tailwind.scss"],
   postcss: {
+    parser: 'postcss-scss',
       plugins: {
           "@tailwindcss/postcss": {},
       },
